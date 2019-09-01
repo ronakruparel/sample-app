5e3654fe79cf7233021a3c11cb9493e6c03a8638
ronakruparel <ronak.ruparel@accionlabs.com>
2019-09-01 12:33:28 +0530

    feat(app):adde styles to login

--- src/components/LoginComponent/index.js
+++ src/components/LoginComponent/index.js
@@ -1,23 +1,31 @@
 import React from "react";
+import "./style.css";
+import Logo from "../../assets/images/logo.png";
 const LoginComponent = ({ handleChange, handleSubmit, username, password }) => {
   return (
-    <div>
-      <label>Username</label>
-      <input
-        type="text"
-        name="username"
-        value={username}
-        onChange={handleChange}
-      />
-      <br />
-      <label>Password</label>
-      <input
-        type="password"
-        name="password"
-        value={password}
-        onChange={handleChange}
-      />
-      <button onClick={handleSubmit}>Submit</button>
+    <div className="loginWrapper">
+      <img className="logo" alt="Logo" src={Logo} />
+      <div className="inputWrapper">
+        <h4>Login to get Started</h4>
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
+      </div>
+      <button className="loginButton" onClick={handleSubmit}>
+        Log In
+      </button>
     </div>
   );
 };
